bb02b49d0a2c559b6d0fb1eb4961e05b7d0b7179
Shah Poojan Dikeshkumar <shahpoojandikeshkumar@poojan.shah-C02D30Q6MD6W>
2023-06-15 16:30:31 +0530

    updated repo9

--- build-component.sh
+++ build-component.sh
@@ -8,26 +8,6 @@ TEMP=$3
 PUSH=${TEMP:=-NO}
 IMAGE=docker-bakery-system/${COMPONENT_NAME}:${TAG}
 
-#NEXUS_USER=${NEXUS_USER}
-#NEXUS_PASSWORD=${NEXUS_PASSWORD}
-#mkdir -p /root/.pip
-#cat > pip.conf << EOF
-#[global]
-#extra-index-url = https://${NEXUS_USER}:${NEXUS_PASSWORD}@prod-nexus.sprinklr.com/nexus/repository/spypi/simple/
-#index-url = https://pypi.python.org/simple
-#EOF
-
-function usage() {
-  echo "./build-component.sh <component> <docker-tag> <push?>"
-  echo "component = one among  [test-component1_repo9
-                                test-component2_repo9
-                                test-component3_repo9]"
-  echo "docker-tag = tag for the image (default latest)"
-  echo "push? = YES or NO (push the image to repo)
-                default:NO
-                "
-  exit 1
-}
 
 function ping(){
     echo "Building from docker file path::"
@@ -56,21 +36,8 @@ function check_component() {
 function build()
 {
     docker build -t ${IMAGE} -f "${DOCKERFILE_PATH}" .
-    case ${PUSH} in
-    YES)
-    echo "Pushing Image:: " ${IMAGE}
-    docker push ${IMAGE}
-    ;;
-    NO)
-    ;;
-    *)
-    echo "Invalid component" && exit 1 ;;
-    esac
 }
 
-if [ "$#" < 3 ]; then
-  usage
-fi
 
 check_component
 build
