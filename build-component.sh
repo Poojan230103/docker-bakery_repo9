bfb8dd5f07b9ee43b959ca889d0161200274f99f
Shah Poojan Dikeshkumar <shahpoojandikeshkumar@poojan.shah-C02D30Q6MD6W>
2023-07-02 22:42:33 +0530

    updated repo9

--- build-component.sh
+++ build-component.sh
@@ -6,8 +6,8 @@ TAG_TEMP=$2
 TAG=${TAG_TEMP:=1.0}
 TEMP=$3
 PUSH=${TEMP:=-NO}
-IMAGE=docker-bakery-system/${COMPONENT_NAME}:${TAG}
-
+# IMAGE=docker-bakery-system/${COMPONENT_NAME}:${TAG}
+IMAGE=poojan23/docker-bakery-system_${COMPONENT_NAME}:${TAG}
 
 function ping(){
     echo "Building from docker file path::"
@@ -15,13 +15,13 @@ function ping(){
 function check_component() {
    echo "COMPONENT_NAME: " ${COMPONENT_NAME}
   case ${COMPONENT_NAME} in
-    test-component1_repo9)
+    repo9-component1)
     DOCKERFILE_PATH=./components_repo9/test-component1_repo9/Dockerfile
     ;;
-    test-component2_repo9)
+    repo9-component2)
     DOCKERFILE_PATH=./components_repo9/test-component2_repo9/Dockerfile
     ;;
-    test-component3_repo9)
+    repo9-component3)
     DOCKERFILE_PATH=./components_repo9/test-component3_repo9/Dockerfile
     ;;
     *)
